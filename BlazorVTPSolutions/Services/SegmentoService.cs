160cfc5953d46f6785dc977754983fbbd140e291
ivanlmpk <ivanlempek@hotmail.com>
2023-10-20 17:25:48 -0300

    Refatorado o SegmentoService

--- BlazorVTPSolutions/Services/SegmentoService.cs
+++ BlazorVTPSolutions/Services/SegmentoService.cs
@@ -12,7 +12,7 @@ namespace BlazorVTPSolutions.Services
     public class SegmentoService : ISegmentoService
     {
         private readonly HttpClient _httpClient;
-        private List<Segmento> Segmentos { get; set; }
+        private List<Segmento>? Segmentos { get; set; }
 
         public SegmentoService(HttpClient httpClient)
         {
@@ -25,26 +25,50 @@ namespace BlazorVTPSolutions.Services
             {
                 return Segmentos;
             }
-            var response = await _httpClient.GetAsync("/api/v1/dataset/64420e4bb510ceea93e0b0ed/611edbd7fd5915f2ae005dc2?apiKey=64420e39b510ceea93e0b0ea&pageSize=30");
-            
-            response.EnsureSuccessStatusCode();
-            var jsonResult = await response.Content.ReadAsStringAsync();
-            JObject jObject = JObject.Parse(jsonResult);
-            var values = jObject.SelectToken("data").ToString();
+
+            var jsonResult = await FetchDataFromApiAsync();
+            var values = ParseResultFromApi(jsonResult); 
+
             if (string.IsNullOrWhiteSpace(values))
                 return new List<Segmento>();
 
-            var segmentos = JsonConvert.DeserializeObject<List<Segmento>>(values);
-
             // armazena a lista de segmentos na propriedade Segmentos
+            var segmentos = GetSegmentosFromJson(values);
             Segmentos = segmentos;
+
+            return segmentos; 
+        }
+
+        private async Task<string> FetchDataFromApiAsync()
+        {
+            var response = await _httpClient.GetAsync("/api/v1/dataset/64420e4bb510ceea93e0b0ed/611edbd7fd5915f2ae005dc2?apiKey=64420e39b510ceea93e0b0ea&pageSize=30");
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        private string ParseResultFromApi(string jsonResult)
+        {
+            JObject jObject = JObject.Parse(jsonResult);
+            var data = jObject["data"];
+
+            if (data == null)
+            {
+                throw new ArgumentException("O JSON fornecido não contém o campo 'data'.");
+            }
+
+            return data.ToString();
+        }
+
+        private List<Segmento> GetSegmentosFromJson(string values)
+        {
+            var segmentos = JsonConvert.DeserializeObject<List<Segmento>>(values);
 
             foreach (var segmento in segmentos)
             {
                 segmento.SetOidFromIdJObject();
             }
 
-            return segmentos; ;
+            return segmentos;
         }
     }
 }
